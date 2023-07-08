de16d496acec7db7a89e5baeb4587a891c0d94a5
Lewis Mendez <briaulin@io.builders.com>
2023-07-08 20:08:28 +0200

    Updated snapshot

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -3,6 +3,7 @@ import App from "../App";
 
 describe(`<${App.name}/>`, () => {
   it("should render without crash", () => {
+    apiMocks.getPodcast("ok");
     const { container } = render(<App />);
     expect(container).toMatchSnapshot();
   });
